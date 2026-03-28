42a7d5c7d58827af854e9b08b557379ac9d6ca2f
Kỳ <40734986+itskyf@users.noreply.github.com>
2026-03-28 15:52:13 +0700

    chore: init SolidStart with Vitest

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { solidStart } from "@solidjs/start/config";
+import { nitroV2Plugin as nitro } from "@solidjs/vite-plugin-nitro-2";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	plugins: [solidStart(), nitro()],
+});
